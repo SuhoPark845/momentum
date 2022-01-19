9690b01f3896f2e49bcd81c3e394b5ae31b929d3
Suho Park <psooh1219@naver.com>
2022-01-19 21:36:35 +0900

    크리스마스까지 남은 날짜 계산기

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,40 @@
 const clock = document.querySelector("h2#clock");
 
-function getClock() {
+function getTimeUntilChristmas() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  const thisYear = date.getFullYear();
+
+  const christmasDate = new Date(`${thisYear}-12-25 00:00:00`);
+
+  const nowDate = new Date();
+  const diffDate = christmasDate - nowDate;
+
+  const millisecondsOfOneDay = 24 * 60 * 60 * 1000;
+  const millisecondsOfOneHour = 60 * 60 * 1000;
+  const millisecondsOfOneMinute = 60 * 1000;
+  const millisecondsOfOneSecond = 1000;
+
+  let diffDay = Math.floor(diffDate / millisecondsOfOneDay);
+  let remainingTimeForToday = diffDate - diffDay * millisecondsOfOneDay;
+
+  let diffHour = Math.floor(remainingTimeForToday / millisecondsOfOneHour);
+  remainingTimeForToday =
+    remainingTimeForToday - diffHour * millisecondsOfOneHour;
+
+  let diffMinute = Math.floor(remainingTimeForToday / millisecondsOfOneMinute);
+  remainingTimeForToday =
+    remainingTimeForToday - diffMinute * millisecondsOfOneMinute;
+
+  let diffSecond = Math.floor(remainingTimeForToday / millisecondsOfOneSecond);
+  remainingTimeForToday =
+    remainingTimeForToday - diffSecond * millisecondsOfOneSecond;
+
+  diffDay = String(diffDay).padStart(3, "0");
+  diffHour = String(diffHour).padStart(2, "0");
+  diffMinute = String(diffMinute).padStart(2, "0");
+  diffSecond = String(diffSecond).padStart(2, "0");
+  clock.innerText = `${diffDay}d ${diffHour}h ${diffMinute}m ${diffSecond}s`;
 }
 
-getClock();
-setInterval(getClock, 1000);
-// setTimeout(sayHello, 1000);
+getTimeUntilChristmas();
+setInterval(getTimeUntilChristmas, 1000);
